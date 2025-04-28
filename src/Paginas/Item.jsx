6ff1054ca6714c652c7e4f1910fc462f715ff88b
Gustavo <gustavo.estudo017@gmail.com>
2025-04-28 10:55:48 -0300

    item

--- src/Paginas/Item.jsx
+++ src/Paginas/Item.jsx
@@ -31,14 +31,47 @@ function Item() {
     return <div className="text-center mt-5"><h2>Erro ao carregar detalhes do produto.</h2></div>;
   }
 
-  const dadosApli  = dados.aplicacoes;
-  
+  const dadosApli = dados.aplicacoes;
+
   return (
-    <div className="card p-4 mb-4">
-      <h2>{dados.nomeProduto}</h2>
-      <p>Marca: {dados.marca}</p>
-      <p>Modelo: {dadosApli[0]?.modelo}</p>
-      <img src={dados.imagemReal} alt={dados.nomeProduto} />
+    <div className="card p-4 mb-4 item">
+      <div className="detalhe">
+        <div className="detalheImg">
+          <img src={dados.imagemReal} alt={dados.nomeProduto} />
+        </div>
+
+        <div className="detalheInfos">
+          <h2>{dados.nomeProduto}</h2>
+          <p>Marca: {dados.marca}</p>
+        </div>
+      </div>
+
+      <div className="Modelo">
+        <h1 className="modeloTitulo">
+          Carros em que essa peça se aplica
+        </h1>
+
+        <div className="modeloItem">
+          <h2>Modelo</h2>
+          <div className="modeloCarro">
+            <p>Carro: {dadosApli[0]?.carroceria}</p>
+            <p>Modelo: {dadosApli[0]?.modelo}</p>
+            <p>Montadora: {dadosApli[0]?.montadora}</p>
+            <p>Versão: {dadosApli[0]?.versao}</p>
+          </div>
+
+          <h2>Dados técnicos</h2>
+          <div className="modeloTeacnico">
+            <p>Motor: {dadosApli[0]?.motor}</p>
+            <p>Combustível: {dadosApli[0]?.combustivel}</p>
+            <p>Potência: {dadosApli[0]?.hp}</p>
+            <p>Cilindrada: {dadosApli[0]?.cilindros}</p>
+            <p>Linha: {dadosApli[0]?.linha}</p>
+            <p>Inicio da fabricação: {dadosApli[0]?.fabricacaoInicial}</p>
+            <p>Fim da Fabricação: {dadosApli[0]?.fabricacaoFinal}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
