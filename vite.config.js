a4861f4d7b233e47efe85137dfa4bca3cda97b83
Gustavo <gustavo.estudo017@gmail.com>
2025-08-21 13:18:04 -0300

    direcionamento para a api da web

--- vite.config.js
+++ vite.config.js
@@ -6,21 +6,13 @@ export default defineConfig({
   server: {
     proxy: {
       '/autocomplete': process.env.VITE_API_URL,
-      '/buscar': process.env.VITE_API_URL,
-      '/tratados': process.env.VITE_API_URL,
-      '/produto': process.env.VITE_API_URL,
-      '/item': process.env.VITE_API_URL,
-      '/similares': process.env.VITE_API_URL,
-      '/lojas': process.env.VITE_API_URL,
-      '/rota-entrega': process.env.VITE_API_URL
+      '/montadoras': process.env.VITE_API_URL,
+      '/pesquisar': process.env.VITE_API_URL,
+      '/produto_detalhes': process.env.VITE_API_URL,
       // '/autocomplete': 'http://127.0.0.1:5000/',
-      // '/buscar': 'http://127.0.0.1:5000/',
-      // '/tratados': 'http://127.0.0.1:5000/',
-      // '/produto': 'http://127.0.0.1:5000/',
-      // '/item': 'http://127.0.0.1:5000/',
-      // '/similares': 'http://127.0.0.1:5000/',
-      // '/lojas': 'http://127.0.0.1:5000/',
-      // '/rota-entrega': 'http://127.0.0.1:5000/',
+      // '/montadoras': 'http://127.0.0.1:5000/',
+      // '/pesquisar': 'http://127.0.0.1:5000/',
+      // '/produto_detalhes': 'http://127.0.0.1:5000/',
     }
   }
 })
