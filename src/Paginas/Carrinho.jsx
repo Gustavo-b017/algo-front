feafc5dbe63b03c4da82fe9217fd1ee452ae5d4f
Gustavo <gustavo.estudo017@gmail.com>
2025-09-29 13:56:13 -0300

    s

--- src/Paginas/Carrinho.jsx
+++ src/Paginas/Carrinho.jsx
@@ -12,8 +12,8 @@ import '/public/style/cardCarrinho.scss';
 import '/public/style/footer.scss';
 import '/public/style/produtoDestaque.scss';
 
-const API_URL = import.meta.env.VITE_API_URL;
-//const API_URL = 'http://127.0.0.1:5000';
+// const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = 'http://127.0.0.1:5000';
 
 function Carrinho() {
     const [produtos, setProdutos] = useState([]);
